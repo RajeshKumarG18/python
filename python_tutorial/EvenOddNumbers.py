d02a90511be85d8401881a5c829e9c374480fd42
RajeshKumarG <rajesh.kumar@techpearl.com>
2025-06-05 19:14:07 +0530

    Added EvenOddNumbers.py

---
+++ python_tutorial/EvenOddNumbers.py
@@ -0,0 +1,11 @@
+# 1. Take a number and assign it to variable number
+# 2. check if number is exactly divided by 2
+# 3. If yes, print "Even Number"
+# 4. If no, print "Odd Number"
+
+number = 1005
+
+if (number % 2 == 0):
+    print("Given number is Even")
+else:
+    print("Given number is odd")

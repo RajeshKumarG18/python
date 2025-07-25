1428494e644017f809017d623124b2797131efb0
RajeshKumarG18 <rajeh.kumar@techpearl.com>
2025-07-25 18:52:12 +0530

    Added basic_datatype.py

--- python_tutorial/basic_datatype.py
+++ python_tutorial/basic_datatype.py
@@ -1,13 +1,13 @@
-a = 10              # integer(int)
+a = 10 # integer(int)
 print(a)
 
-b = 2.5             # floating point(float)
+b = 2.5 # floating point(float)
 print(b)
 
-c = 1 + 3.1j        # complex number(complex)
+c = 1 + 3.1j # complex number(complex)
 print(c)
 
-d = "Hello World"   # string(str)
+d = "Hello World" # string(str)
 print(d)
 
 e = [1,2,"3",4,5.0] # list(list)
@@ -16,8 +16,8 @@ print(e)
 f = (1,2,"3",4,5.0) # tuple(tuple)
 print(f)
 
-g = {'a':5, 2:'b'}  # dictionary(dict)
+g = {'a':5, 2:'b'} # dictionary(dict)
 print(g)
 
-h = True            # boolean(bool)
+h = True # boolean(bool)
 print(h)
